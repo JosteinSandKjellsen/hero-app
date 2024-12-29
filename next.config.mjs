047cb6ab4ed4c8d3ad0eb7d6a9773a9bf3e49ca7
JosteinSandKjellsen <jostein.kjellsen@bouvet.no>
2024-12-29 14:40:32 +0100

    Add experimental server components configuration and ignore specific webpack warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['node:buffer', 'punycode'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.ignoreWarnings = [
+        { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+        { module: /node_modules\/punycode\/punycode\.js/ }
+      ];
+    }
+    return config;
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
