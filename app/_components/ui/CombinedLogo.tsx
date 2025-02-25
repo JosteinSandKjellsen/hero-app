b5feeb8065612f84431a7595a08d282db93ae009
JosteinSandKjellsen <jostein.kjellsen@bouvet.no>
2025-02-25 15:46:29 +0100

    Update CombinedLogo to use WEBP format for improved performance

--- app/_components/ui/CombinedLogo.tsx
+++ app/_components/ui/CombinedLogo.tsx
@@ -10,7 +10,7 @@ export function CombinedLogo({ alt }: CombinedLogoProps): JSX.Element {
   return (
     <div className="w-16 h-16 relative animate-float-subtle">
       <Image
-        src="/images/superheroes/heroapp-icon.png"
+        src="/images/superheroes/heroapp-icon.webp"
         alt={alt}
         fill
         sizes="64px"
